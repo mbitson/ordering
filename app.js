068a22e10c2545a498a0e569cddfbfcaab5599aa
mbitson <importune@gmail.com>
2015-02-12 22:07:35 -0500

    Continued development

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 // Declare app level module which depends on views, and components
 angular.module('ordering', [
   'ngRoute',
-  'ordering.orders',
-  'ordering.order'
+  'ngMaterial'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/orders'});
-}]);
+}])
+.controller('AppCtrl', function( $scope ) {
+        $scope.selectedIndex = 0;
+});
